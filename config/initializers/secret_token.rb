cf042b6d2e5beda5d311c8a103f5be1d57c4b6d4
Training <training@tddcomputer1.local>
2011-01-31 15:09:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoutbox::Application.config.secret_token = '5fa178154a71f3d7a4d9af51bedc526fbea1501fa0fc8ef858b1f20b855db55e65c00b6b5b3722658e6935489b203851da7760b1a3bec5b762e32a3f34f61221'
